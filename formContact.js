10e998f720e720e5c27794cb9578dff7e0082784
Guillaume Andreux <guillaume@andreux.fr>
2022-04-27 09:22:25 +0200

    modification exo form contact

--- formContact.js
+++ formContact.js
@@ -1,3 +1,5 @@
+let champOk = [];
+let nomOk, prenomOk, emailOk, telephoneOk;
 function verif(id, regex) {
     let test;
     if (id == "nom" || id == "prenom") {
@@ -10,66 +12,77 @@ function verif(id, regex) {
         $("#" + id + "Invalide").removeClass("d-none");
         $("#" + id + "Valide").addClass("d-none");
         $("#" + id).addClass("inputInvalid");
-        $("#email").removeClass("inputValid");
+        $("#" + id).removeClass("inputValid");
+        return false;
     } else {
         $("#" + id + "Invalide").addClass("d-none");
         $("#" + id + "Valide").removeClass("d-none");
         $("#" + id).addClass("inputValid");
         $("#" + id).removeClass("inputInvalid");
+        return true;
     }
 }
 
 /* vérification nom */
 $("#nom").keyup(function () {
     verif("nom");
+    champOk[] = verif("nom");
 });
 
 
 /* vérification prénom */
 $("#prenom").keyup(function () {
     verif("prenom");
+    champOk[] = verif("prenom");
 });
 
 /* vérification email */
 $("#email").keyup(function () {
     let regexemail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/;
     verif("email", regexemail);
-
+    champOk[] = verif("email", regexemail);
 });
 
 /* vérification téléphone numéro français */
 $("#telephone").keyup(function() {
     let regextelephone = /^(\d){10}$/;
     verif("telephone", regextelephone);
+    champOk[] = verif("telephone", regextelephone);
 });
 
 /* vérification message */
 $("#message").keyup(function () {
-    let texte = $(this).val();
-    let nombreMots = $.trim(texte).split(" ");
-    $("#nombreCaracteres span").text(texte.length);
-    $("#nombreMots span").text(nombreMots.length);
+    let nombreCaracteres = $(this).val().length;
+    let mots = $.trim($(this).val()).split(" ");
+    mots = mots.filter(function(f) {return f != ""});
+    $("#nombreCaracteres span").text(nombreCaracteres);
+    $("#nombreMots span").text(mots.length);
     if (!$("#message").val()) {
         $("#nombreMots span").text("0");
     }
     if ($("#message").val()) {
         $("#messageInvalide").addClass("d-none");
     }
+    champOk[] = !$("#message").val();
 });
 
-$("#message").keypress(function () {
-    if($(this).val().length >= 200){
-            return false;
-    }
-})
+// $("#message").keypress(function () {
+//     if($(this).val().length >= 200){
+//         return false;
+//     }
+
+// })
+
+    console.log(champOk);
 
 /* soumission */
 $("button").click(function(e) {
     e.preventDefault();
+    console.log(champOk);
     if (!($("#message").val())) {
         $("#messageInvalide").removeClass("d-none");
     } 
-    if ($("#nom").val() && $("#prenom").val() && $("#emailInvalide").hasClass("d-none") && $("#telephoneInvalide").hasClass("d-none") && $("#message").val()) {
+    if (champOk) {
         $("form").addClass("d-none");
         $("#merci").removeClass("d-none");
         $("#merci span").text($("#prenom").val());
